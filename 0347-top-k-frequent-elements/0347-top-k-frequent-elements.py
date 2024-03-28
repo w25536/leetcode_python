de7877e433636bcb79697f1111795c4bd4d7c558
Jeongho Suh <43588796+w25536@users.noreply.github.com>
2024-03-28 16:49:48 +0900

    Time: 88 ms (64.17%), Space: 22.1 MB (6.20%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -24,7 +24,7 @@ class Solution:
         for i in range(len(freq)-1, 0, -1):
             for n in freq[i]: #you are getting array of array so double looping
                 ans.append(n)
-                if len(ans) == k: #you need to stop adding on to ans
+                if len(ans) == k: #you need to stop adding elem until k and return ans
                     return ans
             
           
